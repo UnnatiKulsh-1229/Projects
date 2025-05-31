c9b474d715f892ec70b54b381fc2d76563cbc7a2
Unnati Kulshrestha <unkl.dolly@gmail.com>
2025-05-31 23:42:14 +0530

    Update password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -1,49 +1,27 @@
-rock = '''
-    _______
----'   ____)
-      (_____)
-      (_____)
-      (____)
----.__(___)
-'''
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
-paper = '''
-    _______
----'   ____)____
-          ______)
-          _______)
-         _______)
----.__________)
-'''
-
-scissors = '''
-    _______
----'   ____)____
-          ______)
-       __________)
-      (____)
----.__(___)
-'''
-images=[rock,paper,scissors]
+print("Welcome to the PyPassword Generator!")
+nr_letters = int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
 import random
-user_ch=int(input("0 for rock,1 for paper and 2 for scissors \n"))
-print(images[user_ch])
-sys_ch=random.randint(0,2)
-print("Computer chose")
-print(images[sys_ch])
-if user_ch==0 and sys_ch==2:
-    print('you win')
-elif sys_ch>user_ch:
-    print('you lose')
-elif sys_ch==user_ch:
-    print("Tie")
-elif sys_ch==1 and user_ch==2:
-    print("You win!")
-elif sys_ch==0 and user_ch==2:
-    print("System wins!")
-elif user_ch==1 and sys_ch==0:
-    print("you win !")
-else:
-    print("invalid")
+pswd=''
+pswdlist=[]
+for i in range(1,nr_letters+1):
+    pswdlist+=random.choice(letters)
+for i in range(1,nr_symbols+1):
+    pswdlist+=random.choice(symbols)
+for i in range(1,nr_numbers+1):
+    pswdlist+=random.choice(numbers)
+print(pswdlist)
+random.shuffle(pswdlist)
+print("Password is:",pswdlist)
+for char in pswdlist:
+    pswd+=char
+print("Password is:", pswd)
+
+
 
 
